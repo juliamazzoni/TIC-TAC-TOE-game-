3784a1445ae34450317881d9bbf6ccf71a12a236
Julia Mazzoni <juliarmazzoni@gmail.com>
2023-12-21 17:40:13 +1100

    working on tic tac toe game

--- game.js
+++ game.js
@@ -9,7 +9,6 @@ let dataArrO = [];
 let newArray =[];
 const arrWin = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]];
 
-
 //DOM elements 
 
 const boxesElem = document.querySelectorAll('.boxes');
@@ -25,46 +24,38 @@ playerXElem.classList.add('player-turn');
 
 //handle functions 
 
-
 for (let box of boxesElem){
     box.addEventListener('click', handleClick);
 }
 
 function handleClick(event){
-    
     console.log(numOfGames);
     let box = event.target;
     box.disabled = true;
-    
-    
     
     if (playerTurn === 0){
         playerOElem.classList.add('player-turn');
         playerXElem.classList.remove('player-turn');
         box.textContent = "X";
-        box.style.color='lightgray';
+        box.style.color='white';
         box.classList.add('clicked');
         dataArrX.push(Number(box.dataset.num));
         
-                if (checkWinNum(arrWin, dataArrX)){
-                
-                // dataArrX = newArray;
-                message.textContent = `X wins`;
-                counterX = counterX + 1;
-                scoreX.textContent = counterX;
-
-                for (let box of boxesElem){
-                    box.disabled = true;
-                    for (let numbers of dataArrX){
-                        if(Number(box.dataset.num) === numbers){
-                            console.log('hi');
-                            box.classList.add('win-color');
-                        }
+        if (checkWinNum(arrWin, dataArrX)){
+            message.textContent = `X wins`;
+            counterX = counterX + 1;
+            scoreX.textContent = counterX;
+            for (let box of boxesElem){
+                box.disabled = true;
+                for (let numbers of dataArrX){
+                    if(Number(box.dataset.num) === numbers){
+                        console.log('hi');
+                        box.classList.add('win-color');
                     }
                 }
-                setTimeout(reset,1000);
+            }
+            setTimeout(reset,1000);
         }  
-                
         playerTurn++;
         
     }else if(playerTurn === 1){
@@ -75,33 +66,28 @@ function handleClick(event){
         box.classList.add('clicked');
         dataArrO.push(Number(box.dataset.num));
         
-    
-        // for(let i = 0; i<arrWin.length; i++){
-        //     if (checkWinCondition(dataArrO, arrWin[i])){
-                
-                if (checkWinNum(arrWin, dataArrO)){
-                message.textContent = `O wins`;
-                counterO = counterO + 1;
-                scoreO.textContent = counterO;
+        if (checkWinNum(arrWin, dataArrO)){
+            message.textContent = `O wins`;
+            counterO = counterO + 1;
+            scoreO.textContent = counterO;
 
-                for (let box of boxesElem){
-                    box.disabled = true;
-                    for (let numbers of dataArrO){
-                        if(Number(box.dataset.num) === numbers){
-                            box.classList.add('win-color');
-                        }
+            for (let box of boxesElem){
+                box.disabled = true;
+                for (let numbers of dataArrO){
+                    if(Number(box.dataset.num) === numbers){
+                        box.classList.add('win-color');
                     }
                 }
-                setTimeout(reset,1000);
             }
-            playerTurn--;
+            setTimeout(reset,1000);
+        }
+        playerTurn--;
     }
 
-    // for a draw 
+    // draw 
 
     if (document.querySelectorAll('.clicked').length === boxesElem.length){
         message.textContent = `It's a draw`;
-        console.log('hi');
         setTimeout(reset,1000);
     }
 }
@@ -117,7 +103,7 @@ function checkWinNum(array1, array2){
                     newArray.push([array1[i][j],array1[i][j+1],array1[i][j+2]]);
                     array2 = newArray;
                     return true;
-                }
+            }
         }
     }
 }
@@ -157,6 +143,10 @@ function playerTurnColor(){
 //   })
 // }
 
+// const isSubset = (array1, array2) =>
+//   array2.every((element) => array1.includes(element));
+
+
 // function checkWinNum(array){
 //     if(array.includes(1) && array.includes(2) && array.includes(3)||
 //         array.includes(4) && array.includes(5) && array.includes(6)||
@@ -172,16 +162,8 @@ function playerTurnColor(){
 //         }
 // }
 
-// function checkWinNum(array1, array2){
-//     for(let i=0; i<array1.length; i++){
-//         if(array2.includes(array1[i]));{
-//             return true;
-//         }
-//     }
-// }
 
-// const isSubset = (array1, array2) =>
-//   array2.every((element) => array1.includes(element));
+
 
 
 
